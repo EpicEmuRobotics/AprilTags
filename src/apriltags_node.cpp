7ec9b6b02b9a3ac19cab85b9a9421829713af8df
Shawn Hanna <shawnkarlhanna@gmail.com>
2013-11-25 01:46:47 -0500

    Added 'can' detection to april tags node

--- src/apriltags_node.cpp
+++ src/apriltags_node.cpp
@@ -18,7 +18,17 @@ int main(int argc, char** argv){
 
   ros::NodeHandle n;
 
-  std::vector<tf::TransformBroadcaster> tags_broadcaster(10);
+  /*
+  std::vector<tf::TransformBroadcaster> tags_broadcaster;
+  for (int i = 0; i < NUM_APRIL_TAGS; ++i)
+  {
+    tags_broadcaster.push_back(tf::TransformBroadcaster());
+  }
+  */
+ 
+  ros::Publisher tags_pub = n.advertise<geometry_msgs::PoseStamped>("/apriltags", 1000);
+
+  tf::TransformBroadcaster tags_broadcaster;
 
   AprilTagReader reader;
   reader.setup();
@@ -51,28 +61,39 @@ int main(int argc, char** argv){
       //Yaw = x axis
       //Roll = y axis
 
-      pitch += PI;
-      pitch = -pitch;
+      //pitch += PI;
+      //pitch = -pitch;
 
       geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromRollPitchYaw(yaw, roll, pitch);
 
       //first, we'll publish the transform over tf
-      geometry_msgs::TransformStamped odom_trans;
-      odom_trans.header.stamp = current_time;
-      odom_trans.header.frame_id = "camera_link";
+      geometry_msgs::TransformStamped tag_transform;
+      tag_transform.header.stamp = current_time;
+      tag_transform.header.frame_id = "camera_link";
 
       stringstream ss;
       ss << "april_tag["<<id<<"]";
       
-      odom_trans.child_frame_id = ss.str().c_str();
+      tag_transform.child_frame_id = ss.str().c_str();
 
-      odom_trans.transform.translation.x = x;
-      odom_trans.transform.translation.y = y;
-      odom_trans.transform.translation.z = z;
-      odom_trans.transform.rotation = odom_quat;
+      tag_transform.transform.translation.x = x;
+      tag_transform.transform.translation.y = y;
+      tag_transform.transform.translation.z = z;
+      tag_transform.transform.rotation = odom_quat;
 
       //send the transform
-      tags_broadcaster[id].sendTransform(odom_trans);
+      tags_broadcaster.sendTransform(tag_transform);
+
+      ///trash can has been found
+      if (id % 2 == 0)
+      {
+        geometry_msgs::PoseStamped ps;
+        ps.header.frame_id = "2";
+        ps.pose.position.x = x;
+        ps.pose.position.y = y;
+        ps.pose.orientation = odom_quat;
+        tags_pub.publish(ps);
+      }
     }
 
     cvWaitKey(10);
